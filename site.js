dd3d68205da95e597b2168c0e08ea48d56e0b82b
AvaP3 <84456250+avapierini@users.noreply.github.com>
2023-03-20 14:55:35 +0000

    milestone 2

--- site.js
+++ site.js
@@ -34,7 +34,9 @@ const vue_app = Vue.createApp({
           // This holds your movies.json data.
           movies: [],
           /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-       
+        title: "IMDB + My Top 8 Movies",
+        owner:"Ava Pierini"
+        github: "https://github.com/avapierini/movieproject"
     }
   },
     methods: {
